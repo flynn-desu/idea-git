cc0c67b0336be951dbbc0265a3e1b299807fc564
hfy <3341336443@qq.com>
2026-06-06 13:32:40 +0800

    项目初始化

---
+++ src/main/java/com/flynn/javagit/JavaGitApplication.java
@@ -0,0 +1,13 @@
+package com.flynn.javagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flynn/javagit/JavaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flynn.javagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
